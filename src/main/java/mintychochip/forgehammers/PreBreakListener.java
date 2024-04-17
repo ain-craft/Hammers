00dc0bb7daaf5f363e3a6d4f9b96e28ffcd60b4b
mintychochip <jlo2@csub.edu>
2024-04-17 03:29:36 -0700

    more functional programming

--- src/main/java/mintychochip/forgehammers/PreBreakListener.java
+++ src/main/java/mintychochip/forgehammers/PreBreakListener.java
@@ -1,15 +1,40 @@
 package mintychochip.forgehammers;
 
+import java.util.function.Predicate;
 import mintychochip.forgehammers.container.ForgeHammers;
+import mintychochip.forgehammers.container.Hammer;
 import mintychochip.forgehammers.events.HammerBreakEvent;
 import mintychochip.forgehammers.events.HammerPreBreakEvent;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 
 public final class PreBreakListener extends AbstractListener {
+
+  Predicate<HammerPreBreakEvent> blockIsUnbreakable = event -> {
+    Material type = event.getBlock().getType();
+    return type == Material.BEDROCK;
+  };
+  Predicate<HammerPreBreakEvent> blockTooHard = event -> {
+    Material type = event.getBlock().getType();
+    return type.getHardness() > event.getHammer().getStrength();
+  };
+
+  Predicate<HammerPreBreakEvent> blockBlacklisted = event -> event.getHammer()
+      .blockBlacklisted(event.getBlock());
+  Predicate<HammerPreBreakEvent> blockIsOre = event -> {
+    Material type = event.getBlock().getType();
+    return Constants.ORE_MATERIALS.contains(type);
+  };
+  Predicate<HammerPreBreakEvent> blockIsLowerThanOriginHardness = event -> {
+    float originHardness = event.getOriginHardness();
+    return originHardness < event.getBlock().getType().getHardness();
+  };
+  private final Predicate<HammerPreBreakEvent> check = blockIsUnbreakable.or(blockTooHard)
+      .or(blockBlacklisted).or(blockIsOre).or(blockIsLowerThanOriginHardness);
 
   public PreBreakListener(ForgeHammers instance) {
     super(instance);
@@ -26,61 +51,15 @@ public final class PreBreakListener extends AbstractListener {
       return;
     }
     Bukkit.getPluginManager()
-        .callEvent(new HammerBreakEvent(event.getPlayer(),event.getItemStack(),event.getBlock()));
-  }
-
-  @EventHandler(priority = EventPriority.LOW)
-  private void onBlockIsUnbreakable(final HammerPreBreakEvent event) {
-    if(event.isCancelled()) {
-      return;
-    }
-    Material type = event.getBlock().getType();
-    if (type == Material.BEDROCK) {
-      event.setCancelled(true);
-    }
+        .callEvent(new HammerBreakEvent(event.getPlayer(), event.getItemStack(), event.getBlock()));
   }
 
   @EventHandler(priority = EventPriority.HIGH)
-  private void onOriginHardnessIsHigher(final HammerPreBreakEvent event) {
+  private void checkHandler(final HammerPreBreakEvent event) {
     if (event.isCancelled()) {
       return;
     }
-    float originHardness = event.getOriginHardness();
-    float hardness = event.getBlock().getType().getHardness();
-    Bukkit.broadcastMessage(originHardness + " " + hardness);
-    if (originHardness < event.getBlock().getType().getHardness()) {
-      Bukkit.broadcast(Component.text("hardness cancelled"));
-      event.setCancelled(true);
-    }
-  }
-
-  @EventHandler(priority = EventPriority.LOW)
-  private void onBlockIsTooHard(final HammerPreBreakEvent event) {
-    if (event.isCancelled()) {
-      return;
-    }
-    Material type = event.getBlock().getType();
-    if (type.getHardness() > event.getHammer().getStrength()) {
-      event.setCancelled(true);
-    }
-  }
-  @EventHandler(priority = EventPriority.LOWEST)
-  private void onBlockIsBlacklisted(final HammerPreBreakEvent event) {
-    if (event.isCancelled()) {
-      return;
-    }
-    if (event.getHammer().blockBlacklisted(event.getBlock())) {
-      event.setCancelled(true);
-    }
-  }
-
-  @EventHandler(priority = EventPriority.HIGH)
-  private void onBlockBreakOre(final HammerPreBreakEvent event) {
-    if (event.isCancelled()) {
-      return;
-    }
-    Material type = event.getBlock().getType();
-    if (Constants.ORE_MATERIALS.contains(type)) {
+    if (check.test(event)) {
       event.setCancelled(true);
     }
   }
